28461157452582ec3bfb51ea8f33b17b7f539fc7
Sam McCarthy <samrmccarthy6@gmail.com>
2021-05-25 22:50:09 +1000

    Added three tests to veryify API.
    Added three tests to check that the basic attrivutes of the games by date endpoint can be reached.

--- spec/score_analyzer_spec.rb
+++ spec/score_analyzer_spec.rb
@@ -3,8 +3,18 @@
 require 'score_analyzer'
 
 describe ScoreAnalyzer do
-  it 'should analyze that there are 25 games' do
+  it 'should return the arena Barclays Center' do
     score_analyzer = ScoreAnalyzer.new(ENV.fetch('RAPIDAPI_KEY'))
-    expect(score_analyzer.analyze).to eq '25 games closest score 1 1'
+    expect(score_analyzer.game_details(0, 'arena')).to eq 'Barclays Center'
+  end
+
+  it 'should return the status of the game as Finished' do
+    score_analyzer = ScoreAnalyzer.new(ENV.fetch('RAPIDAPI_KEY'))
+    expect(score_analyzer.game_details(4, 'statusGame')).to eq 'Finished'
+  end
+
+  it 'should return the city is Phoenix' do
+    score_analyzer = ScoreAnalyzer.new(ENV.fetch('RAPIDAPI_KEY'))
+    expect(score_analyzer.game_details(3, 'city')).to eq 'Phoenix'
   end
 end
